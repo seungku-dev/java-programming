c5d0fbbd5ba3d948267be4442be94a1950fb90f4
seungku-dev <seungku.dev@gmail.com>
2023-01-11 01:18:50 +0900

    OOP Class constructor

---
+++ OOP/ClassPart1/src/BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+
+    private String name;
+    private double balance;
+
+    public BankAccount() {
+        // constructor channing
+        this("unknown", 0);
+    }
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- OOP/ClassPart1/src/Main.java
+++ OOP/ClassPart1/src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        Car car = new Car();
-        car.setBrand("Porsche");
-        car.setModel("Carrera");
-        car.setDoors(2);
-        car.setConvertible(true);
-        car.setColors("black");
-
-        car.descrebeModel();
-
-        Car targa = new Car();
-        targa.setBrand("Porsche");
-        targa.setModel("Targa");
-        targa.setDoors(2);
-        targa.setConvertible(false);
-        targa.setColors("red");
-
-        targa.descrebeModel();
+        // Default value set up by constructor channing
+        BankAccount account = new BankAccount();
+        System.out.println(account.getBalance());
+        System.out.println(account.getName());
     }
 }
