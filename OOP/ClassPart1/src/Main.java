48741f426514645dd8ac4c301102321b877911be
seungku-dev <seungku.dev@gmail.com>
2023-01-12 18:00:34 +0900

    OOP Inheritance

--- OOP/ClassPart1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Default value set up by constructor channing
-        BankAccount account = new BankAccount();
-        System.out.println(account.getBalance());
-        System.out.println(account.getName());
-    }
-}
