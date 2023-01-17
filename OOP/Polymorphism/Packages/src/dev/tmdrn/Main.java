42b732639fcf21ee1842f6819098cff22a8ae033
seungku-dev <seungku.dev@gmail.com>
2023-01-18 01:36:11 +0900

    OOP, packages

---
+++ OOP/Polymorphism/Packages/src/dev/tmdrn/Main.java
@@ -0,0 +1,13 @@
+package dev.tmdrn;
+
+import com.abc.first.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        // com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+        System.out.println(firstItem);
+    }
+}
